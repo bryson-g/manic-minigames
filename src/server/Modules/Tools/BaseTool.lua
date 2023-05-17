dcfa9d4b371e6632fc2de56c55d5fecf93e276f4
Bryson G <114206517+bryson-g@users.noreply.github.com>
2023-05-16 21:22:44 -0700

    ✨ add class modules for tool implementation

---
+++ src/server/Modules/Tools/BaseTool.lua
@@ -0,0 +1,24 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Players = game:GetService("Players")
+
+local BaseTool = {}
+BaseTool.__index = BaseTool
+
+function BaseTool.new(player, tool)
+	local self = setmetatable({}, BaseTool)
+
+	self.tool = tool:Clone()
+	self.player = player :: Player
+
+	self:_initTool()
+	return self
+end
+
+function BaseTool:_initTool()
+	local motor = self.tool:FindFirstChildOfClass("Motor6D")
+	motor.Part1 = self.tool.Base
+	motor.Part0 = self.player.Character[motor.Name]
+	self.tool.Parent = self.player.Backpack
+end
+
+return BaseTool
