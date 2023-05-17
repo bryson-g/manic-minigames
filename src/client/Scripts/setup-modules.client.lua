25e554b54866352ec6db0c829d1e6ca013425f92
Bryson G <114206517+bryson-g@users.noreply.github.com>
2023-05-16 21:24:16 -0700

    ⚰️ remove old draft module setup

--- src/client/Scripts/setup-modules.client.lua
+++ src/client/Scripts/setup-modules.client.lua
@@ -1,6 +1,4 @@
 _G.Modules = game:GetService("StarterPlayer").StarterPlayerScripts.Modules
 
-require(_G.Modules.ToolController):_setup()
-
 game.Players.LocalPlayer.CharacterAdded:Wait()
 require(_G.Modules.Camera.CameraController).new()
