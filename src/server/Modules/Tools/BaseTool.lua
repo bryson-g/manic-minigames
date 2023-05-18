c060ab705414a8eaf44730f6bee8e24b7204db24
Bryson G <114206517+bryson-g@users.noreply.github.com>
2023-05-17 21:59:14 -0700

    ✨ add communication for client tool setup

--- src/server/Modules/Tools/BaseTool.lua
+++ src/server/Modules/Tools/BaseTool.lua
@@ -9,6 +9,7 @@ function BaseTool.new(player, tool)
 
 	self.tool = tool:Clone()
 	self.player = player :: Player
+	self.eventRemote = ReplicatedStorage.Remotes.ToolEvents
 
 	self:_initTool()
 	return self
@@ -19,6 +20,8 @@ function BaseTool:_initTool()
 	motor.Part1 = self.tool.Base
 	motor.Part0 = self.player.Character[motor.Name]
 	self.tool.Parent = self.player.Backpack
+
+	self.eventRemote:FireClient(self.player, "CLIENT_SETUP", self.tool)
 end
 
 return BaseTool
